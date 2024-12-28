a86eeb67ad2181907c6a66db023ec5c644a319be
Mirkan Kaçan <kacanmirkan@gmail.com>
2024-12-28 17:54:13 +0300

    update gitignore

--- KocakBlog.UI/Program.cs
+++ KocakBlog.UI/Program.cs
@@ -18,16 +18,15 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
       name: "areas",
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-).WithStaticAssets();
+);
 
 app.Run();
